516c6024fe11177296002c3f308030c7aee60de7
Pierre-Adrien Vasseur <32339081+Pierrad@users.noreply.github.com>
2021-06-20 00:55:48 +0200

    :sparkles:  Add possibility to add/remove notifier

--- utils.py
+++ utils.py
@@ -32,3 +32,28 @@ def writeTaskToLogFile(task):
     taskFileLog = open("taskLog.txt", "a+")
     taskFileLog.write(str(task.get("id")) + " " + str(task.get("due").get("date")) + "\n")
     taskFileLog.close()
+
+
+def saveIdToFileNotifier(id):
+    notifierFile = open("notifier.txt", "r+")
+    lines = notifierFile.readlines()
+    for line in lines:
+        print(line)
+        if (line == str(id) + "\n"):
+            return False
+    notifierFile.write(str(id) + "\n")
+    notifierFile.close()
+    return True
+
+def removeIdToFileNotifier(id):
+    notifierFile = open("notifier.txt", "r")
+    lines = notifierFile.readlines()
+    notifierFile.close()
+
+    notifierFile = open("notifier.txt", "w")
+    for line in lines:
+        if (line != str(id) + "\n"):
+            notifierFile.write(str(id) + "\n")
+    
+    return True
+
