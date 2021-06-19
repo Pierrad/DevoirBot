8d1f4be4e0492e0aaecae391f615489dad6b0832
Pierre-Adrien Vasseur <32339081+Pierrad@users.noreply.github.com>
2021-06-20 00:54:14 +0200

    Add possibility to add/remove notifier & add scheduled task

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
+from discord.ext import tasks
 import os
-from keep_alive import keep_alive
+import keep_alive
 from APIHandler import APIHandler
-from utils import extractFromMessage, writeTaskToLogFile
+from utils import extractFromMessage, writeTaskToLogFile, saveIdToFileNotifier,removeIdToFileNotifier
 
 client = discord.Client()
 
@@ -11,6 +12,7 @@ todo = APIHandler(os.environ['TODOIST_API'], "https://api.todoist.com/rest/v1")
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    send_message.start("Toutes les minutes")
     await client.get_channel(851848071544569859).send("Hola, je suis prêt !")
 
 @client.event
@@ -18,22 +20,44 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if not message.content.startswith('!todoBot'):
+    if not message.content.startswith('!devoirBot'):
         return
 
-    if message.content.startswith('!todoBot help'):
+    if message.content.startswith('!devoirBot help'):
         help = "Commande actuellement disponible :\n- addTask \"ProjectName\" \"TaskContent\" \"Date\" "
         await message.channel.send(help)
 
-    if message.content.startswith('!todoBot addTask'):
+    if message.content.startswith('!devoirBot addTask'):
         (projectID, taskContent, dueDate) = extractFromMessage(message, todo)
         result = todo.createTask(taskContent, projectID, dueDate)
 
         writeTaskToLogFile(result)
 
         await message.channel.send("Okay")
+    
+    if message.content.startswith('!devoirBot addNotifier'):
+        id = message.author.id
+        res = saveIdToFileNotifier(id)
+        if (res):
+            await message.channel.send(f"<@{id}> save")
+        else:
+             await message.channel.send(f"<@{id}> fail")
+    
+    if message.content.startswith('!devoirBot removeNotifier'):
+        id = message.author.id
+        res = removeIdToFileNotifier(id)
+        if (res):
+            await message.channel.send(f"<@{id}> remove")
+        else:
+             await message.channel.send(f"<@{id}> fail")
+    
 
 
-keep_alive()
+@tasks.loop(minutes=1)
+async def send_message(message):
+    await client.get_channel(851848071544569859).send(message)
+
+
+keep_alive.keepAlive()
 client.run(os.environ['TOKEN'])
 
