c5f40d028e74c5b372891f64a8f33f81c6eff28c
Pierre-Adrien Vasseur <32339081+Pierrad@users.noreply.github.com>
2021-06-27 19:24:01 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import re
+from datetime import date, timedelta, datetime
+
 
 def extractFromMessage(message, api):
     task = re.findall('"([^"]*)"',  message.content)
@@ -57,3 +59,44 @@ def removeIdToFileNotifier(id):
     
     return True
 
+def getNotifier():
+    notifierFile = open("notifier.txt", "r")
+    notifier = ""
+    for line in notifierFile.readlines():
+        lineWithoutCariage = line.replace("\n", "")
+        notifier += f"<@{lineWithoutCariage}>, "
+    return notifier
+
+def checkForNextTask(dueDate = ""):
+    if dueDate == "":
+        dateNow = date.today()
+        delay = timedelta(days=1)
+        dueDate = dateNow + delay
+    taskFile = open("taskLog.txt", "r")
+    tasks = []
+    for line in taskFile.readlines():
+        if str(dueDate) in line:
+            tasks.append(line.split(" ")[0])
+    return tasks
+
+def formatTasksOutput(tasks):
+    output = ""
+    for task in tasks:
+        output += "- En " + task[0] + ", il faut : " + task[1] + " pour le " + task[2] + "\n"
+    return output
+
+def removePastTasks():
+    dateNow = date.today()
+    taskFileLog = open("taskLog.txt", "r")
+    removeTasks = []
+    for line in taskFileLog.readlines():
+        if dateNow > datetime.strptime(line.split(" ")[1], '%y-%m-%d'):
+            removeTasks.append(line)
+    taskFileLog.close()
+    
+    taskFileLog = open("taskLog.txt", "w")
+    for line in taskFileLog.readlines:
+        for task in removeTasks:
+            if line != task:
+                taskFileLog.write(line)
+    taskFileLog.close()
