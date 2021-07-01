1e0c6934560be49bb4deaff26e12a4926b613f63
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-07-01 21:22:03 +0200

    :bug: fix bug

--- backend/keep_alive.py
+++ backend/keep_alive.py
@@ -6,7 +6,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api
 from flask_cors import CORS #comment this on deployment
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
