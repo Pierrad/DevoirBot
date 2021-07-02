1051ad07167fa4411e775567ae9ce02a2da7347a
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-07-02 09:18:53 +0200

    :bug: fix

--- backend/keep_alive.py
+++ backend/keep_alive.py
@@ -17,7 +17,8 @@ def home(path):
 
 def run():
   port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0',port=port)
+  if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=port)
 
 
 def keepAlive():
